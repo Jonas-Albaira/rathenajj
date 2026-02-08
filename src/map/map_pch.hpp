761d660a7eb2910bcc4b82d5c41dddcf170375fb
Danilo Lemes <daniloleemes@hotmail.com>
2026-02-08 21:52:54 +0000

    try unity compilation

---
+++ src/map/map_pch.hpp
@@ -0,0 +1,39 @@
+// Copyright (c) rAthena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef MAP_PCH_HPP
+#define MAP_PCH_HPP
+
+#include <array>
+#include <bitset>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <common/cbasetypes.hpp>
+#include <common/mmo.hpp>
+#include <common/random.hpp>
+#include <common/showmsg.hpp>
+#include <common/timer.hpp>
+#include <common/utilities.hpp>
+
+#include "battle.hpp"
+#include "clif.hpp"
+#include "elemental.hpp"
+#include "guild.hpp"
+#include "homunculus.hpp"
+#include "itemdb.hpp"
+#include "map.hpp"
+#include "mercenary.hpp"
+#include "mob.hpp"
+#include "npc.hpp"
+#include "party.hpp"
+#include "pc.hpp"
+#include "script.hpp"
+#include "skill.hpp"
+#include "status.hpp"
+#include "unit.hpp"
+
+#endif // MAP_PCH_HPP
